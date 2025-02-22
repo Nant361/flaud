d5adf0cfd529cfd8bc9fca0698ef7a26929b2a8c
Nant361 <ridhorinantaa45@gmail.com>
2025-02-22 15:49:19 +0700

    Add files via upload

---
+++ Configuration/Setting.js
@@ -0,0 +1,10 @@
+/*
+         Created by Zephyrine 
+        Telegram : @rainoneday
+   All Right Reversed By Zephyrine
+*/
+
+module.exports = {
+    TokenS: "7007962298:AAElqBOu-tDDpmjHb4jymRmyZDCGTBrF4ts",
+    admins: ["5705926766"]
+};
---
+++ set.js
@@ -0,0 +1,9 @@
+module.exports = {
+    ChannelUsername: "@rainonetime",
+    githubToken: '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',
+    githubRepo: 'ZephyrineNasution/databasex',
+    ownerId: '7740268821',
+    githubResellerFilePath: 'resellers.json',
+    githubFilePath: 'trashissue.json'
+};
+
